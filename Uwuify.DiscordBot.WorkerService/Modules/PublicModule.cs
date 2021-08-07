04ee902962a6e9246342a8678a68f4995c192a74
Nick <nickmartin1ee7@gmail.com>
2021-08-06 21:34:31 -0400

    Implemented first command

---
+++ Uwuify.DiscordBot.WorkerService/Modules/PublicModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+using Uwuify.Humanizer;
+
+namespace Uwuify.DiscordBot.WorkerService.Modules
+{
+    public class PublicModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("uwuify")]
+        [Alias("uwu", "owo")]
+        public Task UwuAsync([Remainder] string text) =>
+            Task.FromResult(text.Uwuify());
+    }
+}
