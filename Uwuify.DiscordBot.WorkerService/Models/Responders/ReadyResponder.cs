5831b50bc03dd8d978d805eed9b03f3c243e5f2f
Nicholas M Martin <nmmart04@louisville.edu>
2021-12-19 23:59:38 -0500

    Update application commands on ready

--- Uwuify.DiscordBot.WorkerService/Models/Responders/ReadyResponder.cs
+++ Uwuify.DiscordBot.WorkerService/Models/Responders/ReadyResponder.cs
@@ -3,10 +3,13 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Remora.Discord.API.Abstractions.Gateway.Events;
 using Remora.Discord.API.Abstractions.Objects;
+using Remora.Discord.API.Abstractions.Rest;
 using Remora.Discord.API.Gateway.Commands;
 using Remora.Discord.API.Objects;
+using Remora.Discord.Commands.Services;
 using Remora.Discord.Gateway;
 using Remora.Discord.Gateway.Responders;
+using Remora.Discord.Rest.API;
 using Remora.Results;
 using Uwuify.DiscordBot.WorkerService.Extensions;
 
@@ -17,12 +20,20 @@ public class ReadyResponder : IResponder<IReady>
     private readonly ILogger<ReadyResponder> _logger;
     private readonly DiscordGatewayClient _discordGatewayClient;
     private readonly DiscordSettings _settings;
+    private readonly SlashService _slashService;
+    private readonly IDiscordRestGuildAPI _guildApi;
 
-    public ReadyResponder(ILogger<ReadyResponder> logger, DiscordGatewayClient discordGatewayClient, DiscordSettings settings)
+    public ReadyResponder(ILogger<ReadyResponder> logger,
+        DiscordGatewayClient discordGatewayClient,
+        DiscordSettings settings,
+        SlashService slashService,
+        IDiscordRestGuildAPI guildApi)
     {
         _logger = logger;
         _discordGatewayClient = discordGatewayClient;
         _settings = settings;
+        _slashService = slashService;
+        _guildApi = guildApi;
     }
 
     public async Task<Result> RespondAsync(IReady gatewayEvent, CancellationToken ct = new())
@@ -37,6 +48,21 @@ public class ReadyResponder : IResponder<IReady>
         });
 
         _discordGatewayClient.SubmitCommand(updateCommand);
+
+        foreach (var guild in gatewayEvent.Guilds)
+        {
+            var updateResult = await _slashService.UpdateSlashCommandsAsync(guild.GuildID, ct);
+
+            if (!updateResult.IsSuccess)
+            {
+                var guildResult = await _guildApi.GetGuildAsync(guild.GuildID, ct: ct);
+                var guildName = guildResult.Entity.Name;
+
+                _logger.LogWarning("Failed to update application commands on guild: {guild} ({guildId})",
+                    guildName,
+                    guild.GuildID);
+            }
+        }
 
         return Result.FromSuccess();
     }
