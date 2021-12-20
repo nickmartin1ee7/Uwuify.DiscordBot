d2cef25fce8ab63e2d832fb01b242d2a0055ff03
Nicholas M Martin <nmmart04@louisville.edu>
2021-12-19 21:38:29 -0500

    Refactored entry point

--- Uwuify.DiscordBot.WorkerService/Program.cs
+++ Uwuify.DiscordBot.WorkerService/Program.cs
@@ -9,97 +9,96 @@ using Remora.Rest.Core;
 using Serilog;
 using System;
 using System.Threading.Tasks;
-using Remora.Discord.Gateway.Extensions;
 using Uwuify.DiscordBot.WorkerService.Commands;
 using Uwuify.DiscordBot.WorkerService.Models;
 
-namespace Uwuify.DiscordBot.WorkerService
+namespace Uwuify.DiscordBot.WorkerService;
+public static class Program
 {
-    public static class Program
+    public static async Task Main(string[] args)
     {
-        public static async Task Main(string[] args)
-        {
-            var configuration = new ConfigurationBuilder()
-#if DEBUG
-                .AddJsonFile("appsettings.Development.json")
-#else
-                .AddJsonFile("appsettings.json")
-#endif
-                .AddEnvironmentVariables()
-                .Build();
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
 
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Seq("http://seq:80",
-                    apiKey: configuration.GetSection("Serilog")
-                        .GetValue<string>("ApiKey"))
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Seq("http://seq:80",
+                apiKey: configuration.GetSection("Serilog")
+                    .GetValue<string>("ApiKey"))
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
 
+        var token = configuration
+            .GetSection(nameof(DiscordSettings))
+            .GetValue<string>("Token");
 
-            var host = CreateHostBuilder(configuration, args).Build();
-
-#if DEBUG
-            var debugServerString = host.Services.GetRequiredService<DiscordSettings>().DebugServerId;
-            if (!Snowflake.TryParse(debugServerString, out var debugServer))
+        var host = Host.CreateDefaultBuilder(args)
+            .UseSerilog(Log.Logger)
+            .ConfigureServices(services =>
             {
-                Log.Logger.Warning("Failed to parse debug server from environment");
-            }
-#endif
+                // Discord
+                services
+                    .AddDiscordCommands(true)
+                    .AddCommandGroup<UserCommands>();
 
-            var slashService = host.Services.GetRequiredService<SlashService>();
-
-            var checkSlashSupport = slashService.SupportsSlashCommands();
-            if (!checkSlashSupport.IsSuccess)
-            {
-                Log.Logger.Warning(
-                    "The registered commands of the bot don't support slash commands: {Reason}",
-                    checkSlashSupport.Error!.Message);
-            }
-            else
-            {
-                var updateSlash = await slashService.UpdateSlashCommandsAsync(debugServer);
-                if (!updateSlash.IsSuccess)
-                {
-                    Log.Logger.Warning(
-                        "Failed to update slash commands: {Reason}", 
-                        updateSlash.Error!.Message);
-                }
-            }
-
-            try
-            {
-                await host.RunAsync();
-            }
-            catch (Exception e)
-            {
-                Log.Fatal(e, "Hosted service crashed!");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
-        }
-
-        private static IHostBuilder CreateHostBuilder(IConfiguration configuration, string[] args)
-        {
-            var token = configuration
-                .GetSection(nameof(DiscordSettings))
-                .GetValue<string>("Token");
-
-            return Host.CreateDefaultBuilder(args)
-                .UseSerilog(Log.Logger)
-                .AddDiscordService(_ => token)
-                .ConfigureServices((_, services) =>
-                {
-                    services.AddSingleton(configuration);
-                    services.AddSingleton(configuration
+                // Configuration
+                services
+                    .AddSingleton(configuration)
+                    .AddSingleton(configuration
                         .GetSection(nameof(DiscordSettings))
                         .Get<DiscordSettings>());
-                    services.AddDiscordCommands()
-                        .AddCommandGroup<UserCommands>();
-                    services.AddDiscordGateway(_ => token);
-                    services.AddTransient<SlashService>();
-                });
+            })
+            .AddDiscordService(_ => token)
+            .Build();
+
+        await host.RunAsync();
+
+#if DEBUG
+        await CheckSlashCommandSupport(host);
+#endif
+
+        try
+        {
+            await host.RunAsync();
+        }
+        catch (Exception e)
+        {
+            Log.Fatal(e, "Hosted service crashed!");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+
+    private static async Task CheckSlashCommandSupport(IHost host)
+    {
+        var debugServerString = host.Services.GetRequiredService<DiscordSettings>().DebugServerId;
+
+        if (!Snowflake.TryParse(debugServerString, out var debugServer))
+        {
+            Log.Logger.Warning("Failed to parse debug server from environment");
+        }
+
+        var slashService = host.Services.GetRequiredService<SlashService>();
+
+        var checkSlashSupport = slashService.SupportsSlashCommands();
+        if (!checkSlashSupport.IsSuccess)
+        {
+            Log.Logger.Warning(
+                "The registered commands of the bot don't support slash commands: {Reason}",
+                checkSlashSupport.Error!.Message);
+        }
+        else
+        {
+            var updateSlash = await slashService.UpdateSlashCommandsAsync(debugServer);
+            if (!updateSlash.IsSuccess)
+            {
+                Log.Logger.Warning(
+                    "Failed to update slash commands: {Reason}",
+                    updateSlash.Error!.Message);
+            }
         }
     }
 }
