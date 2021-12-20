60bc297c6aede4e359ef8bae8a5537448dc7f642
Nicholas M Martin <nmmart04@louisville.edu>
2021-12-19 23:28:30 -0500

    Guild joined and count folder move

---
+++ Uwuify.DiscordBot.WorkerService/Models/Responders/GuildJoinedResponder.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.Gateway.Responders;
+using Remora.Results;
+
+namespace Uwuify.DiscordBot.WorkerService.Models.Responders;
+
+public class GuildJoinedResponder : IResponder<IGuildCreate>
+{
+    private readonly ILogger<GuildJoinedResponder> _logger;
+
+    public GuildJoinedResponder(ILogger<GuildJoinedResponder> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<Result> RespondAsync(IGuildCreate gatewayEvent, CancellationToken ct = new())
+    {
+        _logger.LogInformation("Joined new guild: {guildName} ({guildId})",
+            gatewayEvent.Name,
+            gatewayEvent.ID);
+
+        return Result.FromSuccess();
+    }
+}
