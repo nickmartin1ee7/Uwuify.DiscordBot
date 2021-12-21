84655cced159a5f614e2f489daa429fd6d33cf85
Nicholas M Martin <nmmart04@louisville.edu>
2021-12-21 15:17:56 -0500

    [library issue] Guild delete is unknown event

---
+++ Uwuify.DiscordBot.WorkerService/Extensions/UnknownEventExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Rest.Core;
+
+namespace Uwuify.DiscordBot.WorkerService.Extensions
+{
+    public static class UnknownEventExtensions
+    {
+        public static bool TryHandleAsGuildDelete(this IUnknownEvent unknownEvent, out Snowflake guildId)
+        {
+            guildId = default;
+
+            var gatewayEvent = JsonSerializer.Deserialize<CustomUnknownGatewayEvent>(unknownEvent.Data);
+
+            if (gatewayEvent?.GatewayType == "GUILD_DELETE")
+            {
+                guildId = new Snowflake(ulong.Parse(gatewayEvent.UnavailableGuild.Id));
+                return true;
+            }
+
+            return false;
+        }
+
+        internal class UnavailableGuild
+        {
+            [JsonPropertyName("id")]
+            public string Id { get; set; }
+        }
+
+        internal class CustomUnknownGatewayEvent
+        {
+            [JsonPropertyName("t")]
+            public string GatewayType { get; set; }
+
+            [JsonPropertyName("d")]
+            public UnavailableGuild UnavailableGuild { get; set; }
+        }
+    }
+}
---
+++ Uwuify.DiscordBot.WorkerService/Models/Responders/UnknownResponder.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Remora.Discord.API.Abstractions.Gateway.Events;
+using Remora.Discord.API.Abstractions.Rest;
+using Remora.Discord.Gateway.Responders;
+using Remora.Rest.Core;
+using Remora.Results;
+using Uwuify.DiscordBot.WorkerService.Extensions;
+
+namespace Uwuify.DiscordBot.WorkerService.Models.Responders;
+
+public class UnknownResponder : IResponder<IUnknownEvent>
+{
+    private readonly ILogger<UnknownResponder> _logger;
+    private readonly IDiscordRestUserAPI _userApi;
+
+    public UnknownResponder(ILogger<UnknownResponder> logger,
+        IDiscordRestUserAPI userApi)
+    {
+        _logger = logger;
+        _userApi = userApi;
+    }
+
+    public async Task<Result> RespondAsync(IUnknownEvent gatewayEvent, CancellationToken ct = new())
+    {
+        if (gatewayEvent.TryHandleAsGuildDelete(out Snowflake guildId))
+        {
+            if (!ShortTermMemory.KnownGuilds.Contains(guildId))
+                return Result.FromSuccess();
+
+            _logger.LogInformation("Left guild: {guildId}",
+                guildId);
+
+            ShortTermMemory.KnownGuilds.Remove(guildId);
+
+            await _logger.LogGuildCountAsync(_userApi, ct);
+        }
+
+        return Result.FromSuccess();
+    }
+}
