b4aefcfcdf233b360e619dc80e634c05946d79b8
Nicholas M Martin <nmmart04@louisville.edu>
2021-12-25 22:46:46 -0500

    ToString on ShardGroup

--- Uwuify.ClassLibrary/Models/ShardGroup.cs
+++ Uwuify.ClassLibrary/Models/ShardGroup.cs
@@ -1,3 +1,6 @@
 namespace Uwuify.ClassLibrary.Models;
 
-public record ShardGroup(int GroupId, int MaxShards, List<int> ShardIds);
+public record ShardGroup(int GroupId, int MaxShards, List<int> ShardIds)
+{
+    public override string ToString() => $"{nameof(ShardGroup)} {{ {nameof(GroupId)} = {GroupId}, {nameof(MaxShards)} = {MaxShards}, {nameof(ShardIds)} = {{ {string.Join(", ", ShardIds)} }} }}";
+}
