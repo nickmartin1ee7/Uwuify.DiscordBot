6a22a7ec9d3d0d4d37f3cb799c1270791653f34f
Nicholas M Martin <nmmart04@louisville.edu>
2022-02-24 21:42:33 -0500

    Remove old prop

--- Uwuify.DiscordBot.WorkerService/Models/DiscordSettings.cs
+++ Uwuify.DiscordBot.WorkerService/Models/DiscordSettings.cs
@@ -2,7 +2,6 @@
 
 public class DiscordSettings
 {
-    public ulong? OwnerId { get; set; }
     public ulong? DebugServerId { get; set; }
     public string Status { get; set; }
     public string Token { get; set; }
