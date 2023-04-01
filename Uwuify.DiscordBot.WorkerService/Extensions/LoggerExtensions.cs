144210801bbb2438f16c7d189bd998642ae2f502
Nicholas Martin <nickmartin1ee7@gmail.com>
2023-03-31 23:25:52 -0400

    Only log guild count if it is actually different

--- Uwuify.DiscordBot.WorkerService/Extensions/LoggerExtensions.cs
+++ Uwuify.DiscordBot.WorkerService/Extensions/LoggerExtensions.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.Extensions.Logging;
+
 using Uwuify.DiscordBot.WorkerService.Models;
 
 namespace Uwuify.DiscordBot.WorkerService.Extensions
 {
     public static class LoggerExtensions
     {
+        private static int s_lastLoggedGuildCount;
+
         public static void LogGuildCount(this ILogger logger)
         {
+            if (s_lastLoggedGuildCount == ShortTermMemory.KnownGuilds.Count)
+                return;
+
+            s_lastLoggedGuildCount = ShortTermMemory.KnownGuilds.Count;
+
             logger.LogInformation("Guild Count: {count}",
                 ShortTermMemory.KnownGuilds.Count);
         }
